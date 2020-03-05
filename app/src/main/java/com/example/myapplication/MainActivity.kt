2e77b26894b72a750c326f1c1471c5be69cda847
Shatishkumar <shatishkuma-wm17@student.tarc.edu.my>
2020-03-05 12:53:56 +0800

    first command

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,26 +3,45 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var diceImage : ImageView
+    lateinit var diceImage2 : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Get the Button view from the layout and assign a click
+        // listener to it.
         val rollButton: Button = findViewById(R.id.rollbutton)
         rollButton.setOnClickListener { rollDice() }
-        val resulttext: TextView = findViewById(R.id.resulttext)
-        rollButton.setOnClickListener { rollDice() }
+        diceImage = findViewById(R.id.dice_image)
+        diceImage2 = findViewById(R.id.dice_image)
     }
 
+    /**
+     * Click listener for the Roll button.
+     */
     private fun rollDice() {
-        Toast.makeText(this, "button clicked",
-            Toast.LENGTH_SHORT).show()
-        val resultText: TextView = findViewById(R.id.resulttext)
-        resultText.text = "Dice Rolled!"
+        diceImage.setImageResource(getRandomDiceImage())
+        diceImage2.setImageResource(getRandomDiceImage())
+    }
 
+    private fun getRandomDiceImage() : Int {
+        val randomInt = (1..6).random()
+
+        return when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
     }
 }
